4a320a67afc9b96487e315ac5a44e4e55bd46504
Yogi <yogi.it.in@gmail.com>
2021-08-07 16:57:33 +0530

    first commit

---
+++ src/main/java/com/Auth2ServerJwtApplication.java
@@ -0,0 +1,13 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Auth2ServerJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Auth2ServerJwtApplication.class, args);
+	}
+
+}
